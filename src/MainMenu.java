f4aa88779525c80492209065a7444ee8be67ca69
Ludvig Hagberg <luddeha@gmail.com>
2012-04-17 19:54:32 +0200

    Fixed some graphics

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,47 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class MainMenu {
 
+public class MainMenu extends BasicGameState{
+
+	int stateID = -1;
+	
+	/**
+	 * 
+	 * @param stateID Sets the ID o the menu.
+	 */
+	public MainMenu(int stateID){
+		this.stateID = stateID;
+	}
+
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		return stateID;
+	}
+	
+	
 }
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,23 @@
 public class Player {
-
+	String name;
+	String image;
+	int health;
+	
+	public Player(String name, String image){
+		this.name = name;
+		this.image = image;
+		health = 100;
+	}
+	
+	public String name(){
+		return name;
+	}
+	
+	public String image(){
+		return image;
+	}
+	
+	public int health(){
+		return health;
+	}
 }
