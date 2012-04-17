f15f40de4d5cad45e866c9c27bde467d5c7c849e
Axel Samuelsson <limbero@limbero.org>
2012-04-17 19:23:33 +0200

    Networking fixes

--- src/NetworkServerThread.java
+++ src/NetworkServerThread.java
@@ -1,26 +1,23 @@
 import java.io.*;
 import java.net.*;
 
-/**
-   @author Viebrapadata
-*/
 public class NetworkServerThread extends Thread {
     private Socket socket = null;
     private BufferedReader in;
+    private String owner;
+    private String user;
+    
     PrintWriter out;
     public NetworkServerThread(Socket socket) {
         super("NetworkServerThread");
         this.socket = socket;
+        owner = "limbero";
+        user = "luddeha";
     }
 
     private String readLine() throws IOException {
         String str = in.readLine();
-        //System.out.println(""  + socket + " : " + str);
         return str;
-    }
-
-    private boolean validateUser() {
-        return true;
     }
 
     public void run(){
@@ -30,37 +27,24 @@ public class NetworkServerThread extends Thread {
             in = new BufferedReader
                 (new InputStreamReader(socket.getInputStream()));
 	
-            String inputLine, outputLine;
-	
-            int balance = 1000;
-            int value;
-            validateUser();
-            out.println("Welcome to Bank! (1)Balance, (2)Withdrawal, (3)Deposit, (4)Exit"); 
+            String inputLine;
+            
+            boolean matched = false;
+            
+            out.println("host"); 
             inputLine = readLine();
-            int choise = Integer.parseInt(inputLine);
-            while (choise != 4) {
-                int deposit = 1;
-                switch (choise) {
-                case 2:
-                    deposit = -1;
-                case 3:
-                    out.println("Enter amount: ");	
-                    inputLine= readLine();
-                    value = Integer.parseInt(inputLine);
-                    balance += deposit * value;
-                case 1:
-                    out.println("Current balance is " + balance + " dollars");
-                    out.println("(1)Balance, (2)Withdrawal, (3)Deposit, (4)Exit");
-                    inputLine=readLine();
-                    choise = Integer.parseInt(inputLine);
-                    break;
-                case 4:
-                    break;
-                default: 
-                    break;
-                }
+            while(!inputLine.equals("exit")){
+            	if(inputLine.equals(owner)){
+            		System.out.println("right host");
+            		out.println("user");
+            		inputLine = readLine();
+            		matched = true;
+            	}
+            	if(inputLine.equals(user) && matched){
+        			out.println("Welcome "+user);
+        		}
             }
-            out.println("Good Bye");
+            
             out.close();
             in.close();
             socket.close();
